a108f9129dc1c335df583c087a407c45a0a70008
torrapipes <crbergas@gmail.com>
2019-02-20 17:12:36 +0100

    Proyecto maven empezado

---
+++ CatarinaRodriguezBergas/src/main/java/org/lasencinas/CatarinaRodriguezBergas/App.java
@@ -0,0 +1,13 @@
+package org.lasencinas.CatarinaRodriguezBergas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
